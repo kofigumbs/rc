2341da1c5348c279db5d6f592bb2f8819e84f7e8
Kofi Gumbs <h.kofigumbs@gmail.com>
2019-09-30 14:49:05 -0400

    Extract bathroom person variables and use relative paths

--- bathroom-floss/Main.elm
+++ bathroom-floss/Main.elm
@@ -10,32 +10,60 @@ import Svg.Attributes exposing (..)
 
 main : Svg msg
 main =
+    let
+        spacing =
+            0.05
+
+        head =
+            [ cx "5"
+            , cy "1"
+            , r (p (1 - spacing))
+            ]
+
+        body =
+            { limbWidth = 0.85
+            , armLength = 2.0
+            , legLength = 3.0
+            , torsoLength = 3.0
+            , startX = 5.0
+            , startY = 2.0
+            }
+
+        extremity =
+            "a"
+                ++ p (body.limbWidth / 2)
+                ++ p (body.limbWidth / 2)
+                ++ "0 1 1 "
+                ++ p -body.limbWidth
+                ++ "0"
+    in
     Html.main_ []
         [ Html.node "style" [] [ text styles ]
         , svg [ Svg.Attributes.viewBox "0 0 10 10" ]
-            [ circle [ cx "4.9", cy "1", r "0.95", fill "black" ] []
+            [ circle (fill "black" :: head) []
             , Svg.path
-                [ d <| """
-                    M 5 2
-                    H 6
-                    Q 7 2 7 3
-                    V 5
-                    A 0.50 0.50 1 1 1 6 5
-                    V 3
-                    H 5.95
-                    V 9
-                    A 0.50 0.50 1 1 1 4.95 9
-                    V 6
-                    H 4.90
-                    V 9
-                    A 0.5 0.5 1 1 1 3.90 9
-                    V 3
-                    H 3.85
-                    V 5
-                    A 0.50 0.50 1 1 1 2.85 5
-                    V 3
-                    Q 2.85 2 3.85 2
-                """
+                [ d <|
+                    String.join " "
+                        [ "M" ++ p body.startX ++ p body.startY
+                        , "h" ++ p (body.limbWidth + (spacing * 3 / 2))
+                        , "q" ++ p body.limbWidth ++ " 0 " ++ p body.limbWidth ++ p body.limbWidth
+                        , "v" ++ p body.armLength
+                        , extremity
+                        , "v" ++ p -body.armLength
+                        , "h" ++ p -spacing
+                        , "v" ++ p (body.torsoLength + body.legLength)
+                        , extremity
+                        , "v" ++ p -body.legLength
+                        , "h" ++ p -spacing
+                        , "v" ++ p body.legLength
+                        , extremity
+                        , "v" ++ p -(body.torsoLength + body.legLength)
+                        , "h" ++ p -spacing
+                        , "v" ++ p body.armLength
+                        , extremity
+                        , "v" ++ p -body.armLength
+                        , "q 0 " ++ p -body.limbWidth ++ p body.limbWidth ++ p -body.limbWidth
+                        ]
                 ]
                 []
             ]
@@ -63,3 +91,8 @@ styles =
         transform-style: preserve-3d;
     }
     """
+
+
+p : Float -> String
+p float =
+    " " ++ String.fromFloat float ++ " "
