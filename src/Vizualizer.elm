8d569e35f78d1dd6248f356f3c658d3dd10f5a94
Kofi Gumbs <h.kofigumbs@gmail.com>
2019-10-25 14:19:55 -0400

    Vizualize the circle of life

--- src/Vizualizer.elm
+++ src/Vizualizer.elm
@@ -2,10 +2,13 @@ module Vizualizer exposing (main)
 
 import Angle exposing (Angle)
 import Axis3d
+import Browser
+import Browser.Events
 import Camera3d
 import Color
 import Direction3d
 import Html exposing (Html)
+import Json.Decode as D
 import Length
 import Parameter1d
 import Pixels
@@ -18,8 +21,84 @@ import Triangle3d
 import Viewpoint3d
 
 
-main : Html msg
+main : Program () Model Msg
 main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+type alias Model =
+    { activeKeys : List Int
+    }
+
+
+init : () -> ( Model, Cmd Msg )
+init () =
+    ( { activeKeys = [] }, Cmd.none )
+
+
+type Msg
+    = GotKeyDown Int
+    | GotKeyUp Int
+
+
+upper =
+    [ 81, 50, 87, 51, 69, 82, 53, 84, 54, 89, 55, 85, 73, 57, 79, 48, 80, 219, 187, 221 ]
+
+
+lower =
+    [ 90, 83, 88, 68, 67, 86, 71, 66, 72, 78, 74, 77, 188, 76, 190, 186, 191 ]
+
+
+indexOf : a -> List a -> Int
+indexOf value =
+    let
+        recurse index list =
+            case list of
+                [] ->
+                    -1
+
+                first :: rest ->
+                    if first == value then
+                        index
+                    else
+                        recurse (index + 1) rest
+    in
+    recurse 0
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        GotKeyDown code ->
+            let
+                _ =
+                    Debug.log "code:down" code
+            in
+            ( { model | activeKeys = code :: model.activeKeys }, Cmd.none )
+
+        GotKeyUp code ->
+            let
+                _ =
+                    Debug.log "code:up" code
+            in
+            ( { model | activeKeys = List.filter ((/=) code) model.activeKeys }, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch
+        [ Browser.Events.onKeyDown (D.map GotKeyDown (D.field "which" D.int))
+        , Browser.Events.onKeyUp (D.map GotKeyUp (D.field "which" D.int))
+        ]
+
+
+view : Model -> Html Msg
+view model =
     let
         triangle1 =
             Triangle3d.from
@@ -39,11 +118,14 @@ main =
         mesh2 =
             Mesh.triangles [] [ triangle2 ]
 
+        -- z = left right
+        -- y = up down
+        -- x = in out
         viewpoint =
             Viewpoint3d.lookAt
-                { focalPoint = Point3d.meters 0.5 2 0
-                , eyePoint = Point3d.meters 9 5 4
-                , upDirection = Direction3d.z
+                { focalPoint = Point3d.meters 0 2 0
+                , eyePoint = Point3d.meters 10 5 3
+                , upDirection = Direction3d.y
                 }
 
         camera =
@@ -63,17 +145,20 @@ main =
             Axis3d.through (Point3d.meters 0 2 0) Direction3d.x
 
         angles =
-            Parameter1d.leading 16 <|
+            Parameter1d.leading 12 <|
                 Quantity.interpolateFrom
                     (Angle.degrees 0)
                     (Angle.degrees 360)
 
-        rotatedSquare angle =
-            square |> Drawable.rotateAround rotationAxis angle
+        rotatedSquare activeKeys index angle =
+            if List.any (\key -> modBy 12 (indexOf key upper) == index) activeKeys then
+                square |> Drawable.rotateAround rotationAxis angle
+            else
+                Drawable.empty
     in
     Scene3d.unlit []
         { camera = camera
         , width = Pixels.pixels 800
         , height = Pixels.pixels 600
         }
-        (List.map rotatedSquare angles)
+        (List.indexedMap (rotatedSquare model.activeKeys) angles)
