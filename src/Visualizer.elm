d5f9cd339c9d3c5d9260aedf500a8d05314075be
Kofi Gumbs <h.kofigumbs@gmail.com>
2019-11-01 17:51:46 -0400

    Rotating bathroom person

--- src/Visualizer.elm
+++ src/Visualizer.elm
@@ -118,27 +118,18 @@ subscriptions model =
 
 view : Model -> Html msg
 view model =
+    -- TODO add parameters that vary with channel data
     viewSubject model
-        (cycle (Array.get 0 model.channels) vary.colors)
-        (cycle (Array.get 1 model.channels) vary.colors)
-        (cycle (Array.get 2 model.channels) vary.colors)
-        (cycle (Array.get 3 model.channels) vary.colors)
-        (cycle (Array.get 4 model.channels) vary.scales)
-        (cycle (Array.get 5 model.channels) vary.rotations)
-        (cycle (Array.get 6 model.channels) vary.rotations)
-        (cycle (Array.get 7 model.channels) vary.rotations)
 
 
-viewSubject : Model -> Color -> Color -> Color -> Color -> Float -> Angle -> Angle -> Angle -> Html msg
-viewSubject model color1 color2 color3 color4 scale xAngle yAngle zAngle =
+viewSubject : Model -> Html msg
+viewSubject model =
     let
         viewpoint =
-            Viewpoint3d.orbit
-                { groundPlane = SketchPlane3d.xy
-                , azimuth = Angle.degrees 0
-                , elevation = Angle.degrees 0
-                , focalPoint = Point3d.meters 0 0 0
-                , distance = Length.meters 8
+            Viewpoint3d.lookAt
+                { focalPoint = Point3d.meters 0 -2 0
+                , eyePoint = Point3d.meters 0 2 16
+                , upDirection = Direction3d.y
                 }
 
         camera =
@@ -148,29 +139,10 @@ viewSubject model color1 color2 color3 color4 scale xAngle yAngle zAngle =
                 , clipDepth = Length.meters 0.1
                 }
 
-        light1 =
-            Light.directional
-                (Chromaticity.fromColor color1)
+        sunlight =
+            Light.directional Chromaticity.daylight
                 (Illuminance.lux 10000)
-                (Direction3d.yz (Angle.degrees 45))
-
-        light2 =
-            Light.directional
-                (Chromaticity.fromColor color2)
-                (Illuminance.lux 10000)
-                (Direction3d.yz (Angle.degrees 135))
-
-        light3 =
-            Light.directional
-                (Chromaticity.fromColor color3)
-                (Illuminance.lux 10000)
-                (Direction3d.yz (Angle.degrees 225))
-
-        light4 =
-            Light.directional
-                (Chromaticity.fromColor color4)
-                (Illuminance.lux 10000)
-                (Direction3d.yz (Angle.degrees 315))
+                (Direction3d.zxY (Angle.degrees 45) (Angle.degrees 90))
 
         ambientLighting =
             Light.overcast
@@ -178,100 +150,125 @@ viewSubject model color1 color2 color3 color4 scale xAngle yAngle zAngle =
                 , chromaticity = Chromaticity.daylight
                 , zenithLuminance = Luminance.nits 5000
                 }
+
+        swing =
+            Drawable.rotateAround Axis3d.y (Angle.degrees (model.time / 30))
     in
     Scene3d.render [ Scene3d.clearColor Color.black ]
         { camera = camera
         , width = Pixels.pixels model.width
         , height = Pixels.pixels model.height
         , ambientLighting = Just ambientLighting
-        , lights = Scene3d.fourLights ( light1, { castsShadows = False } ) light2 light3 light4
+        , lights = Scene3d.oneLight sunlight { castsShadows = False }
         , exposure = Exposure.fromMaxLuminance (Luminance.nits 10000)
         , whiteBalance = Chromaticity.daylight
         }
-        [ Drawable.physical aluminum subject
-            |> Drawable.scaleAbout Point3d.origin scale
-            |> Drawable.rotateAround Axis3d.x xAngle
-            |> Drawable.rotateAround Axis3d.y yAngle
-            |> Drawable.rotateAround Axis3d.z zAngle
-        ]
-
-
-subject : Mesh a (Mesh.Triangles Mesh.WithNormals Mesh.NoUV Mesh.NoTangents Mesh.ShadowsDisabled)
-subject =
-    let
-        side =
-            Length.meters 0.45
-    in
-    -- Shape.block side side side
-    Shape.sphere { radius = side, subdivisions = 72 }
-
-
-cycle : Maybe Int -> Array b -> b
-cycle channel values =
-    let
-        index =
-            modBy (Array.length values) (Maybe.withDefault 0 channel)
-    in
-    case Array.get index values of
-        Just value ->
-            value
-
-        _ ->
-            -- UNTO INFINITY
-            cycle Nothing values
-
-
-vary =
-    { colors =
-        Array.fromList
-            [ Color.red
-            , Color.orange
-            , Color.yellow
-            , Color.green
-            , Color.blue
-            , Color.purple
-            , Color.grey
+        (List.map swing
+            [ bodyPart head
+            , bodyPart torso
+                |> Drawable.translateIn Direction3d.negativeY
+                    (Quantity.divideBy 2 var.torsoLength
+                        |> Quantity.plus var.headRadius
+                        |> Quantity.plus var.spacing
+                    )
+            , bodyPart arm
+                |> Drawable.rotateAround Axis3d.x (Angle.degrees 90)
+                |> Drawable.translateIn Direction3d.negativeY
+                    (Quantity.plus var.spacing var.headRadius)
+                |> Drawable.translateIn Direction3d.x
+                    (Quantity.divideBy 2 var.limbWidth
+                        |> Quantity.plus var.limbWidth
+                        |> Quantity.plus (Quantity.multiplyBy 2 var.spacing)
+                    )
+            , bodyPart arm
+                |> Drawable.rotateAround Axis3d.x (Angle.degrees 90)
+                |> Drawable.translateIn Direction3d.negativeY
+                    (Quantity.plus var.spacing var.headRadius)
+                |> Drawable.translateIn Direction3d.negativeX
+                    (Quantity.divideBy 2 var.limbWidth
+                        |> Quantity.plus var.limbWidth
+                        |> Quantity.plus (Quantity.multiplyBy 2 var.spacing)
+                    )
+            , bodyPart leg
+                |> Drawable.rotateAround Axis3d.x (Angle.degrees 90)
+                |> Drawable.translateIn Direction3d.negativeY
+                    (Quantity.plus var.spacing var.headRadius
+                        |> Quantity.plus var.torsoLength
+                    )
+                |> Drawable.translateIn Direction3d.x
+                    (Quantity.divideBy 2 var.limbWidth
+                        |> Quantity.plus var.spacing
+                    )
+            , bodyPart leg
+                |> Drawable.rotateAround Axis3d.x (Angle.degrees 90)
+                |> Drawable.translateIn Direction3d.negativeY
+                    (Quantity.plus var.spacing var.headRadius
+                        |> Quantity.plus var.torsoLength
+                    )
+                |> Drawable.translateIn Direction3d.negativeX
+                    (Quantity.divideBy 2 var.limbWidth
+                        |> Quantity.plus var.spacing
+                    )
             ]
-    , scales =
-        Array.fromList
-            [ 1.0
-            , 1.1
-            , 1.25
-            , 1.45
-            , 1.7
-            , 2.0
-            , 2.35
-            , 2.75
-            , 3.2
-            , 3.7
-            ]
-    , rotations =
-        Array.fromList
-            [ Angle.degrees 30
-            , Angle.degrees 15
-            , Angle.degrees 30
-            , Angle.degrees 45
-            , Angle.degrees 60
-            , Angle.degrees 45
-            , Angle.degrees 60
-            , Angle.degrees 75
-            , Angle.degrees 60
-            , Angle.degrees 75
-            , Angle.degrees 90
-            , Angle.degrees 75
-            , Angle.degrees 60
-            , Angle.degrees 75
-            , Angle.degrees 60
-            , Angle.degrees 45
-            , Angle.degrees 60
-            , Angle.degrees 45
-            , Angle.degrees 30
-            ]
+        )
+
+
+head : Mesh a (Mesh.Triangles Mesh.WithNormals Mesh.NoUV Mesh.NoTangents Mesh.ShadowsDisabled)
+head =
+    Shape.sphere
+        { radius = var.headRadius
+        , subdivisions = 72
+        }
+
+
+leg : Mesh a (Mesh.Triangles Mesh.WithNormals Mesh.NoUV Mesh.NoTangents Mesh.ShadowsDisabled)
+leg =
+    Shape.cylinder
+        { radius = Quantity.divideBy 2 var.limbWidth
+        , height = var.legLength
+        , subdivisions = 72
+        }
+
+
+arm : Mesh a (Mesh.Triangles Mesh.WithNormals Mesh.NoUV Mesh.NoTangents Mesh.ShadowsDisabled)
+arm =
+    Shape.cylinder
+        { radius = Quantity.divideBy 2 var.limbWidth
+        , height = var.armLength
+        , subdivisions = 72
+        }
+
+
+extremity : Mesh a (Mesh.Triangles Mesh.WithNormals Mesh.NoUV Mesh.NoTangents Mesh.ShadowsDisabled)
+extremity =
+    Shape.sphere
+        { radius = Quantity.divideBy 2 var.limbWidth
+        , subdivisions = 72
+        }
+
+
+torso : Mesh a (Mesh.Triangles Mesh.WithNormals Mesh.NoUV Mesh.NoTangents Mesh.ShadowsDisabled)
+torso =
+    Shape.block (Quantity.sum [ var.limbWidth, var.spacing, var.limbWidth ])
+        var.torsoLength
+        var.limbWidth
+
+
+bodyPart : Mesh a (Mesh.Triangles Mesh.WithNormals uv tangents shadows) -> Drawable a
+bodyPart =
+    Drawable.physical
+        { baseColor = Color.white
+        , roughness = 0.25
+        , metallic = False
+        }
+
+
+var =
+    { spacing = Length.meters 0.025
+    , headRadius = Length.meters 0.85
+    , limbWidth = Length.meters 0.85
+    , armLength = Length.meters 2.0
+    , legLength = Length.meters 3.0
+    , torsoLength = Length.meters 2.5
+    , movement = Length.meters 0.85
     }
-
-
-{-| <https://github.com/ianmackenzie/elm-3d-scene/blob/85ee07e1454a0270ead17c0319688ccf091ade2f/examples/Common/Materials.elm#L19-L21>
--}
-aluminum : Material
-aluminum =
-    { baseColor = Color.rgb255 233 235 236, roughness = 0.6, metallic = True }
